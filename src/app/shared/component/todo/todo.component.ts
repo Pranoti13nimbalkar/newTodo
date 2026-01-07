d1e3e5e400ea401ba785a442e54278641eab0116
skiranbiradar666 <skiranbiradar666@gmail.com>
2026-01-07 17:09:41 +0530

    create todo features

--- src/app/shared/component/todo/todo.component.ts
+++ src/app/shared/component/todo/todo.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { ITodos } from '../../models/todo';
 import { todoArr } from '../../const/todoConst';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-todo',
@@ -10,12 +11,41 @@ import { todoArr } from '../../const/todoConst';
 export class TodoComponent implements OnInit {
   isInEditMode:boolean=false
 todosArr : Array<ITodos>= todoArr
+
+@ViewChild('todoItem') todoItem !: ElementRef
+  
 trackByTodo(index:number , trackByTodo:ITodos){
   return trackByTodo
 }
-  constructor() { }
+  constructor(private _snackBar : MatSnackBar) { }
 
   ngOnInit(): void {
+
+   
   }
 
+  onTodoAdd(){
+
+    if(this.todoItem.nativeElement.value.length > 0){
+
+      let todoObj : ITodos = {
+      todoItem : this.todoItem.nativeElement.value,
+      todoId :Date.now().toString()
+    }
+    console.log(todoObj)
+    this.todoItem.nativeElement.value = ''
+    this.todosArr.push(todoObj)
+   
+
+    this._snackBar.open('New TodoItem Added Successfully !', 'close', {
+      verticalPosition : 'top',
+      horizontalPosition : 'center',
+      duration : 3000
+    })
+    }
+
+    
+    
+  }
+  
 }
